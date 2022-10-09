0b24b66a139c81aa1e79ae0f4e045f1f87436ef0
Steven N. Wilson <49218333+steven-n-wilson@users.noreply.github.com>
2022-10-09 08:23:09 -0400

    added cartesian methods from abstract class

--- pointcp/design2/PointCP2.java
+++ pointcp/design2/PointCP2.java
@@ -108,6 +108,16 @@ public class PointCP2 extends PointCP5
     return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
   }
 
+  public double getDistance(PointCP3 pointB)
+  {
+    // Obtain differences in X and Y, sign is not important as these values
+    // will be squared later.
+    double deltaX = getX() - pointB.getX();
+    double deltaY = getY() - pointB.getY();
+    
+    return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+  }
+
   /**
    * Rotates the specified point by the specified number of degrees.
    * Not required until E2.30
@@ -123,6 +133,17 @@ public class PointCP2 extends PointCP5
     double Y = getY();
         
     return new PointCP2('C',
+      (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
+      (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y));
+  }
+
+  public PointCP3 rotatePointCartesian(double rotation)
+  {
+    double radRotation = Math.toRadians(rotation);
+    double X = getX();
+    double Y = getY();
+        
+    return new PointCP3('C',
       (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
       (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y));
   }
